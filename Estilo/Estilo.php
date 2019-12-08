f0e604a06f067e8c85e51f093e8ba7856209405c
Luis Henrique <luis.reis@beyondtechnology.solutions>
2019-12-07 22:28:30 -0300

    Mudando encoding do menu para teste.

--- Estilo/Estilo.php
+++ Estilo/Estilo.php
@@ -40,7 +40,7 @@ if (!isset($menu)) {
 <!--chamada para o menu flutuante-->
 
 <div ID="DownLevelDiv">
-     Aguarde um instante, seu browser está carregando o menu da aplicação.
+     Aguarde um instante, seu browser estÃ¡ carregando o menu da aplicaÃ§Ã£o.
 </div>
 
 <script language="JavaScript" src="Funcoes/Toolbar.js"></script>
@@ -63,90 +63,90 @@ if (ToolBar_Supported != null && ToolBar_Supported == true)
 
 addICPMenu("1", "Cachorro","#","","");
 	addICPSubMenu("1","Cadastro","Cachorro_Formulario.php","");
-	addICPSubMenu("1","Relação","Cachorro_Listar.php","");
+	addICPSubMenu("1","RelaÃ§Ã£o","Cachorro_Listar.php","");
 	addICPSubMenu("1","Ninhada Nacional","Ninhada_Formulario.php","");
 	addICPSubMenu("1","Ninhada Estrangeira","NinhadaEstrangeira_Formulario.php","");
-	addICPSubMenu("1","Súmula","Sumula_Formulario.php","");
+	addICPSubMenu("1","SÃºmula","Sumula_Formulario.php","");
 	//addICPSubMenu("2","Cachorros por Canil","Cachorro_ConsultaCanil.php","");
 
 addICPMenu("2", "Canil","#","","");
 	addICPSubMenu("2","Cadastro","Canil_Formulario.php","");
-	addICPSubMenu("2","Relação","Canil_Listar.php","");
+	addICPSubMenu("2","RelaÃ§Ã£o","Canil_Listar.php","");
 addICPMenu("3", "Figurantes","#","","");
 	addICPSubMenu("3","Cadastro","Adestrador_Formulario.php","");
-	addICPSubMenu("3","Relação","Adestrador_Listar.php","");
+	addICPSubMenu("3","RelaÃ§Ã£o","Adestrador_Listar.php","");
 
 addICPMenu("32", "Diretoria","#","","");
 	addICPSubMenu("32","Cadastro","Diretoria_Formulario.php","");
-	addICPSubMenu("32","Relação","Diretoria_Listar.php","");
+	addICPSubMenu("32","RelaÃ§Ã£o","Diretoria_Listar.php","");
 
 
-addICPMenu("31", "Sócios","#","","");
+addICPMenu("31", "SÃ³cios","#","","");
 	addICPSubMenu("31","Cadastro","Socio_Formulario.php","");
-	addICPSubMenu("31","Relação","Socio_Listar.php","");
+	addICPSubMenu("31","RelaÃ§Ã£o","Socio_Listar.php","");
 addICPMenu("4", "Clubes","#","","");
 	addICPSubMenu("4","Cadastro","Clube_Formulario.php","");
-	addICPSubMenu("4","Relação","Clube_Listar.php","");
-addICPMenu("5", "Exposições","#","","");
+	addICPSubMenu("4","RelaÃ§Ã£o","Clube_Listar.php","");
+addICPMenu("5", "ExposiÃ§Ãµes","#","","");
 	addICPSubMenu("5","Cadastro","Exposicoes_Formulario.php","");
-	addICPSubMenu("5","Relação","Exposicoes_Listar.php","");
+	addICPSubMenu("5","RelaÃ§Ã£o","Exposicoes_Listar.php","");
 	addICPSubMenu("5","Resultado CINE","javascript:AbrirRel(31)","");
 	addICPSubMenu("5","Importar Resultado","javascript:AbrirRel(37)","");
 addICPMenu("6", "Provas","#","","");
 	addICPSubMenu("6","Cadastro","Provas_Formulario.php","");
-	addICPSubMenu("6","Relação","Provas_Listar.php","");
-addICPMenu("7", "Proprietários","#","","");
+	addICPSubMenu("6","RelaÃ§Ã£o","Provas_Listar.php","");
+addICPMenu("7", "ProprietÃ¡rios","#","","");
 	addICPSubMenu("7","Cadastro","Proprietario_Formulario.php","");
-	addICPSubMenu("7","Relação","Proprietario_Listar.php","");
-addICPMenu("8", "Juízes","#","","");
+	addICPSubMenu("7","RelaÃ§Ã£o","Proprietario_Listar.php","");
+addICPMenu("8", "JuÃ­zes","#","","");
 	addICPSubMenu("8","Cadastro","Juiz_Formulario.php","");
-	addICPSubMenu("8","Relação","Juiz_Listar.php","");
+	addICPSubMenu("8","RelaÃ§Ã£o","Juiz_Listar.php","");
 
-addICPMenu("10", "Relatórios","#","","");
+addICPMenu("10", "RelatÃ³rios","#","","");
 	addICPSubMenu("10","Registros por Ano","javascript:AbrirRel(0)","");
 	addICPSubMenu("10","Registros por Filiada","javascript:AbrirRel(36)","");
 	addICPSubMenu("10","Chapas por Ano","javascript:AbrirRel(1)","");
 	addICPSubMenu("10","Ninhadas por Ano","javascript:AbrirRel(2)","");
-	addICPSubMenu("10","N° de Canis Por UF","javascript:AbrirRel(4)","");
-	addICPSubMenu("10","Seleções por Ano","javascript:AbrirRel(5)","");
+	addICPSubMenu("10","NÂ° de Canis Por UF","javascript:AbrirRel(4)","");
+	addICPSubMenu("10","SeleÃ§Ãµes por Ano","javascript:AbrirRel(5)","");
 	addICPSubMenu("10","Canis Registrados por Ano","javascript:AbrirRel(7)","");
 	addICPSubMenu("10","Raio X por Ano","javascript:AbrirRel(8)","");
 	addICPSubMenu("10","Reprodutores por Ano","javascript:AbrirRel(9)","");
 	addICPSubMenu("10","Reprodutoras por Ano","javascript:AbrirRel(10)","");
 	addICPSubMenu("10","Mapa de Ninhada por Ano","javascript:AbrirRel(11)","");
 	addICPSubMenu("10","Mapa de Ninhada Ano Canil","javascript:AbrirRel(12)","");
-	addICPSubMenu("10","Sócios por UF","javascript:AbrirRel(13)","");
+	addICPSubMenu("10","SÃ³cios por UF","javascript:AbrirRel(13)","");
 	addICPSubMenu("10","Filhotes Por Pai","javascript:AbrirRel(15)","");
-	addICPSubMenu("10","Filhotes Por Mãe","javascript:AbrirRel(16)","");
-	//addICPSubMenu("10","Pesquisa por N° de Telefone","javascript:AbrirRel(17)","");
-	addICPSubMenu("10","Pais e Filhotes por Período","javascript:AbrirRel(18)","");
-	addICPSubMenu("10","Mães e Filhotes por Período","javascript:AbrirRel(19)","");
+	addICPSubMenu("10","Filhotes Por MÃ£e","javascript:AbrirRel(16)","");
+	//addICPSubMenu("10","Pesquisa por NÂ° de Telefone","javascript:AbrirRel(17)","");
+	addICPSubMenu("10","Pais e Filhotes por PerÃ­odo","javascript:AbrirRel(18)","");
+	addICPSubMenu("10","MÃ£es e Filhotes por PerÃ­odo","javascript:AbrirRel(19)","");
 	addICPSubMenu("10","Pais e Chapas do Filhotes","javascript:AbrirRel(20)","");
-	addICPSubMenu("10","Mães e Chapas do Filhotes","javascript:AbrirRel(21)","");
-	addICPSubMenu("10","Relação Geral de Cachorros","javascript:AbrirRel(22)","");
-	addICPSubMenu("10","Relação Geral de Clubes","javascript:AbrirRel(23)","");
-	addICPSubMenu("10","Relação Geral de Juízes","javascript:AbrirRel(24)","");
-	addICPSubMenu("10","Relação de Proprietários","javascript:AbrirRel(25)","");
-	addICPSubMenu("10","Relação de Adestradores","javascript:AbrirRel(26)","");
-	addICPSubMenu("10","Canis com/sem Débitos","javascript:AbrirRel(27)","");
+	addICPSubMenu("10","MÃ£es e Chapas do Filhotes","javascript:AbrirRel(21)","");
+	addICPSubMenu("10","RelaÃ§Ã£o Geral de Cachorros","javascript:AbrirRel(22)","");
+	addICPSubMenu("10","RelaÃ§Ã£o Geral de Clubes","javascript:AbrirRel(23)","");
+	addICPSubMenu("10","RelaÃ§Ã£o Geral de JuÃ­zes","javascript:AbrirRel(24)","");
+	addICPSubMenu("10","RelaÃ§Ã£o de ProprietÃ¡rios","javascript:AbrirRel(25)","");
+	addICPSubMenu("10","RelaÃ§Ã£o de Adestradores","javascript:AbrirRel(26)","");
+	addICPSubMenu("10","Canis com/sem DÃ©bitos","javascript:AbrirRel(27)","");
 	addICPSubMenu("10","Cachorros por Canil","javascript:AbrirRel(28)","");
-	addICPSubMenu("10","Cachorros por N° SBCPA","javascript:AbrirRel(29)","");
-	addICPSubMenu("10","Cachorros por N° CBKC","javascript:AbrirRel(30)","");
-	addICPSubMenu("10","Súmulas por Ano/Juiz","javascript:AbrirRel(33)","");
-	addICPSubMenu("10","Sintético de Chapas","javascript:AbrirRel(34)","");
-	addICPSubMenu("10","Seleções Vencidas","javascript:AbrirRel(35)","");	
-	addICPSubMenu("10","Sócios por filiadas","javascript:AbrirRel(38)","");	 
+	addICPSubMenu("10","Cachorros por NÂ° SBCPA","javascript:AbrirRel(29)","");
+	addICPSubMenu("10","Cachorros por NÂ° CBKC","javascript:AbrirRel(30)","");
+	addICPSubMenu("10","SÃºmulas por Ano/Juiz","javascript:AbrirRel(33)","");
+	addICPSubMenu("10","SintÃ©tico de Chapas","javascript:AbrirRel(34)","");
+	addICPSubMenu("10","SeleÃ§Ãµes Vencidas","javascript:AbrirRel(35)","");	
+	addICPSubMenu("10","SÃ³cios por filiadas","javascript:AbrirRel(38)","");	 
 
-	//addICPSubMenu("10","Proprietários","javascript:AbrirRel(14)","");
+	//addICPSubMenu("10","ProprietÃ¡rios","javascript:AbrirRel(14)","");
 
 addICPMenu("9", "Tabelas Auxiliares","#","","");
 	addICPSubMenu("9","Adestramento","TBAdestramento_Listar.php","");	
 	addICPSubMenu("9","Categoria","TBCategoria_Listar.php","");
 	addICPSubMenu("9","Cor","TBCor_Listar.php","");
-	addICPSubMenu("9","Qualificação Cão","TBQualificacaoCao_Listar.php","");		
-	addICPSubMenu("9","Qualificação Juiz","TBQualificacaoJuiz_Listar.php","");
+	addICPSubMenu("9","QualificaÃ§Ã£o CÃ£o","TBQualificacaoCao_Listar.php","");		
+	addICPSubMenu("9","QualificaÃ§Ã£o Juiz","TBQualificacaoJuiz_Listar.php","");
 	addICPSubMenu("9","Raio X","TBRaioX_Listar.php","");
-	addICPSubMenu("9","Seleção","TBSelecao_Listar.php","");
+	addICPSubMenu("9","SeleÃ§Ã£o","TBSelecao_Listar.php","");
 	addICPSubMenu("9","Presidente e Diretor","TbPresidenteDiretor_Formulario.php","");
 
 addICPMenu("11", "Sair","","javascript:sair()","");
@@ -210,7 +210,7 @@ function AbrirRel(n)
 	pag[38] = "Rel_Personalizado2.php";
 
 	if (n == 24){
-		if (confirm("Deseja listar somente os Juízes Ativos?")){
+		if (confirm("Deseja listar somente os JuÃ­zes Ativos?")){
 			pag[24] = pag[24] + "?Ativo=1";
 		}
 	}
