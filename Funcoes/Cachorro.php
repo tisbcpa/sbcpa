0afb39e31fb692626171d7b58a930f7b250e28a2
Luis Henrique <luis.reis@beyondtechnology.solutions>
2019-12-11 14:14:26 -0300

    Teste cadastro.

--- Funcoes/Cachorro.php
+++ Funcoes/Cachorro.php
@@ -403,8 +403,8 @@ function CadastrarCachorro($NoCachorro, $TPSexo, $IdCor, $DtNascimento, $NoPai, 
 			exit;
 		}	
 	}else{
-		$columnNascimento = 'NULL';
-		$DtNascimento = ", DaNascimento";
+		$columnNascimento = ", DaNascimento";
+		$DtNascimento = 'NULL';
 	}
 
 	if ($DtRaioX != ""){
