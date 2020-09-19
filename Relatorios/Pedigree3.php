e3ebdd019e9ad1fe92788a555e72958620587924
Luis Henrique Reis <luis.reis@beyondtechnology.solutions>
2020-09-19 17:57:49 -0300

    Add imagem pedigree

--- Relatorios/Pedigree3.php
+++ Relatorios/Pedigree3.php
@@ -317,6 +317,7 @@ for ($i=1; $i<=$Tam; $i++)
 }
 
 $pdf -> ezNewPage();
+$pdf->ezSetMargins(-15,-15,10,-15);
 $pdf->ezImage('/var/www/SIPA/Imagens/Verso_branca.jpg',0,1170,'none','left');
 /*
 //Linhas Verticais
