5303ffd60dfc486f565deb4938727c07c59be879
Luis Henrique Reis <luis.reis@beyondtechnology.solutions>
2020-09-19 17:35:57 -0300

    Add imagem pedigree

--- Relatorios/Pedigree3.php
+++ Relatorios/Pedigree3.php
@@ -159,7 +159,7 @@ $LinhaConvencoes = 1043;
 $pdf =& new Cezpdf('a3','landscape');
 $pdf -> selectFont('../Funcoes/Relatorios/fonts/Times-Roman.afm');
 $pdf->ezSetMargins(0,0,10,0);
-$pdf->ezImage('/var/www/SIPA/Imagens/Frente_branca.jpg',0,1163,'none','left');
+$pdf->ezImage('/var/www/SIPA/Imagens/Frente_branca.jpg',0,1162,'none','left');
 
 //-------------- LADO ESQUERDO DA PRIMEIRA PÁGINA -------------------------------------
 /*
